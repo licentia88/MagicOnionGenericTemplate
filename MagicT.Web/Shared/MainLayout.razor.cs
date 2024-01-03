266ccc1fcb1973734d269c5d096d18472fcf616c
asimgunduz <a.gunduz@live.com>
2024-01-03 09:54:33 +0200

    latest

---
+++ MagicT.Web/Shared/MainLayout.razor.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace MagicT.Web.Shared;
+
+public partial class MainLayout
+{
+    [CascadingParameter(Name = nameof(SignOutFunc))]
+    public Func<Task> SignOutFunc { get; set; }
+
+    bool _drawerOpen = true;
+ 
+    void DrawerToggle()
+    {
+        _drawerOpen = !_drawerOpen;
+    }
+
+    async Task SignOutAsync()
+    {
+        await SignOutFunc.Invoke();
+    }
+}
+
