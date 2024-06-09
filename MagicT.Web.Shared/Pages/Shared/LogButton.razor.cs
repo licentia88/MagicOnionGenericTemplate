1822e06f799ba66b69cead3aabb1174c77c35e66
asimgunduz <a.gunduz@live.com>
2024-06-09 09:47:45 +0300

    logButton

---
+++ MagicT.Web.Shared/Pages/Shared/LogButton.razor.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace MagicT.Web.Shared.Pages.Shared;
+
+public partial class LogButton
+{
+    [Parameter,EditorRequired]
+    public EventCallback OnClick { get; set; }
+}
